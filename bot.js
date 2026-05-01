ea7f23e8a8c37132748ff15f27aa3fdf0ef63060
afnanmeow <afnanmeow@afnanmeow.afnanmeow.org>
2026-05-02 02:04:23 +0600

    debugging for why bot cant login 2

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ const http = require('http');
 
 const TOKEN = process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 //debug
 console.log("ENV CHECK:", {
